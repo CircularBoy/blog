db9906e30d2c1000e6049c01b0f161de789c847a
CircularBoy <--yurets097@gmail.com>
2021-01-25 14:20:14 +0200

    connect redux debug tool

--- src/store/redux.ts
+++ src/store/redux.ts
@@ -9,11 +9,13 @@ const reducers = combineReducers({
 type RootReducerType = typeof reducers
 export type AppStateType = ReturnType<RootReducerType>
 
-// @ts-ignore
-// const reduxDevTools =
-//   // @ts-ignore
-//   typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-// const store = createStore(reducers, compose(applyMiddleware(thunk), reduxDevTools))
-const store = createStore(reducers, compose(applyMiddleware(thunk)))
+let store
+if (typeof window !== 'undefined') {
+  // @ts-ignore
+  const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  store = createStore(reducers, compose(applyMiddleware(thunk), reduxDevTools))
+} else {
+  store = createStore(reducers, compose(applyMiddleware(thunk)))
+}
 
 export default store
